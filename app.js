6a053c326f97807f42cff51345ee8b852f6fc9e6
nikayel <alinikayeljamal@gmail.com>
2023-07-20 11:39:38 -0700

    fix mongose error and use database items instead of temporary items

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 
-app.set('view engine', 'ejs');
-let workItems = [];
-let todos = ["play video game"];
-app.use(bodyparser.urlencoded({extended: true}));  
-app.use(express.static("public"));
+
+//get date and time:
 let day = "";
 app.get("/", function(req,res){
 let today = new Date();
@@ -17,9 +15,57 @@ let option = {
     month: "long"
 };
 
+app.set('view engine', 'ejs');
+// let workItems = [];
+// let todos = ["play video game"];
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-day = today.toLocaleDateString("en-us", option);
-res.render("List", {kindOfDay: day, newListItems: todos});
+//connection for mongoose
+mongoose.connect("mongodb://localhost:27017/todoListDB");
+
+
+const itemSchema = {
+
+  name: String
+};
+const Item = mongoose.model("item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo list."
+});
+
+const item2 = new Item({
+  name: "Hit + button to create a new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+//Storing items into an array
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems)
+  .then(function(){
+    console.log("Saved to database");
+  })
+  .catch(function(err){
+      console.log(err);
+    }
+  );
+  Item.find({}).then(function(foundItems){
+
+
+    day = today.toLocaleDateString("en-us", option);
+    res.render("List", {kindOfDay: day, newListItems: foundItems});
+})
+.catch(function(err){
+  console.log(err);
+});
+
+
+
 
 
 });
@@ -41,7 +87,7 @@ app.post("/", function(req,res){
     }
     console.log(req.body);
 
-   
+
 })
 
 
