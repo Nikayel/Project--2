b10dbca203e6bf99e59558519e95feabebfa6ccb
nikayel <alinikayeljamal@gmail.com>
2023-07-20 13:11:47 -0700

    add header and fotter to saperate ejs files

--- app.js
+++ app.js
@@ -3,17 +3,8 @@ const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 
-
-
-//get date and time:
+// Get date and time:
 let day = "";
-app.get("/", function(req,res){
-let today = new Date();
-let option = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-};
 
 app.set('view engine', 'ejs');
 // let workItems = [];
@@ -21,12 +12,11 @@ app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//connection for mongoose
+// Connection for mongoose
 mongoose.connect("mongodb://localhost:27017/todoListDB");
 
-
+// Define the Mongoose Schema and Model outside of route handler
 const itemSchema = {
-
   name: String
 };
 const Item = mongoose.model("item", itemSchema);
@@ -43,55 +33,65 @@ const item3 = new Item({
   name: "<-- Hit this to delete an item."
 });
 
-//Storing items into an array
+// Storing items into an array
 const defaultItems = [item1, item2, item3];
+//
+// Item.insertMany(defaultItems)
+//   .then(function(){
+//     console.log("Saved to database");
+//   })
+//   .catch(function(err){
+//       console.log(err);
+//     }
+//   );
 
-Item.insertMany(defaultItems)
-  .then(function(){
-    console.log("Saved to database");
-  })
-  .catch(function(err){
+app.get("/", function(req, res){
+  let today = new Date();
+  let option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  Item.find({})
+    .then(function(foundItems) {
+      if(foundItems.length === 0){
+        Item.insertMany(defaultItems)
+          .then(function(){
+            console.log("saved to database");
+          })
+          .catch(function(err){
+            console.log(err);
+        });
+        res.reirect("/");
+      }else{
+        day = today.toLocaleDateString("en-us", option);
+        res.render("List", {kindOfDay: day, newListItems: foundItems});
+      }
+
+    })
+    .catch(function(err) {
       console.log(err);
-    }
-  );
-  Item.find({}).then(function(foundItems){
-
-
-    day = today.toLocaleDateString("en-us", option);
-    res.render("List", {kindOfDay: day, newListItems: foundItems});
-})
-.catch(function(err){
-  console.log(err);
+    });
 });
 
-
-
-
-
+app.get("/work", function(req, res){
+  res.render("List", {kindOfDay: "work List", newListItems: workItems});
 });
-app.get("/work",function(req,res){
-    res.render("List",{kindOfDay: "work List", newListItems: workItems});
-})
 
+app.post("/", function(req, res){
+  let todo = req.body.newItem;
 
+  if(req.body.Button === "work"){
+    workItems.push(todo);
+    res.redirect("/work");
+  } else {
+    todos.push(todo);
+    res.redirect("/");
+  }
+  console.log(req.body);
+});
 
-app.post("/", function(req,res){
-    let todo = req.body.newItem;
-
-    if(req.body.Button === "work"){
-        workItems.push(todo);
-        res.redirect("/work");
-    }else{
-        todos.push(todo);
-        res.redirect("/");
-    }
-    console.log(req.body);
-
-
-})
-
-
-
-app.listen(3000,function(){
-    console.log("server is running on port 3000");
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
 });
